cba11369a61b077f2bf07fa49853fa1777d5a260
jeevithaa16 <49775331+jeevithaa16@users.noreply.github.com>
2021-09-17 03:38:17 +0530

    commit

---
+++ Oracle-JDBC/Create.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+public class Create
+{
+	public static void main(String args[])
+	{
+		try{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection( "jdbc:oracle:thin:@localhost:1521:xe", "system", "1234");  
+		Statement stmt=con.createStatement(); 
+		String sql = "CREATE TABLE MOVIES " +
+                   "(name VARCHAR(255), " + 
+                   " actor VARCHAR(255), " +
+                   " actress VARCHAR(255), " + 
+                   " year INTEGER, " + 
+                   " director VARCHAR(255))";
+
+        stmt.executeUpdate(sql);
+         System.out.println("Table created successfully");
+
+		con.close();  
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.toString());
+		}
+	}
+}
