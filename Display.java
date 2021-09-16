40843aa93af30d78a38da310a1a1dec97213777b
jeevithaa16 <49775331+jeevithaa16@users.noreply.github.com>
2021-09-17 03:35:25 +0530

    initial commit

---
+++ Display.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+public class Display
+{
+	public static void main(String args[])
+	{
+		try{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection( "jdbc:oracle:thin:@localhost:1521:xe", "system", "1234");  
+		Statement stmt=con.createStatement(); 
+		ResultSet rs=stmt.executeQuery("SELECT * FROM MOVIES");
+		System.out.println("Name 	Actor    	Actress  	Year 	Director");
+         
+         while (rs.next()) {
+
+            String name = rs.getString("name");
+            String actor = rs.getString("actor");
+         	String actress = rs.getString("actress");
+         	int year = rs.getInt("year");
+         	String director = rs.getString("director");
+            System.out.println(name+"   "+actor+"    "+actress+"	"+year+"	"+director);
+         }
+
+		con.close();  
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.toString());
+		}
+	}
+}
